6815009f4954ae3c497847f42b575a4d6fd806c7
Beau Shinkle <beaushinkle@gmail.com>
2017-03-31 11:08:55 -0400

    revert fighter to duelist

--- src/sim_5e/fighter.clj
+++ src/sim_5e/fighter.clj
@@ -8,11 +8,12 @@
   [level _]
   (let [main-stat (main-stat level)
         max-hp (+ 12 (* (- level 1) 8))]
-    {:fighter {:damage (roll 1 8 (+ main-stat 0))
+    {:fighter {:damage (roll 1 8 (+ main-stat 2))
                :pc true
                :ac 18
                :init ((roll 20 0))
                :hit (roll 20 (+ main-stat (proficiency level)))
+               :protection-style false
                :attack-advantage false
                :attack-disadvantage false
                :reaction true
