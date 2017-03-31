1c8d26e594376d51a9ce14bf0436d0d87629dad9
Beau Shinkle <beaushinkle@gmail.com>
2017-03-31 11:08:45 -0400

    revert paladin to duelist

--- src/sim_5e/paladin.clj
+++ src/sim_5e/paladin.clj
@@ -9,12 +9,13 @@
   (let [main-stat (main-stat level)
         max-hp (+ 12 (* (- level 1) 8))
         casting-mod (+ 3 (proficiency level))]
-    {:paladin (merge {:damage (roll 1 8 (+ main-stat 0))
+    {:paladin (merge {:damage (roll 1 8 (+ main-stat 2))
                       :pc true
                       :ac 18
                       :init ((roll 20 0))
                       :hit (roll 20 (+ main-stat (proficiency level)))
                       :attacks (if (> level 4) 2 1)
+                      :protection-style false
                       :attack-advantage false
                       :reaction true
                       :attack-disadvantage false
