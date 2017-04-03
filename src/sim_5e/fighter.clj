a783104b4b1c67fa75fafbbd3b7be7c8c044efe9
Beau Shinkle <beaushinkle@gmail.com>
2017-04-03 10:23:33 -0400

    More fighter options

--- src/sim_5e/fighter.clj
+++ src/sim_5e/fighter.clj
@@ -12,7 +12,9 @@
         to-hit (+ main-stat proficiency)
         light-attack {:num 1 :sides 6 :mod main-stat :hit to-hit}
         melee-attack {:num 1 :sides 8 :mod (+ main-stat 0) :hit to-hit}
-        two-handed {:num 2 :sides 6 :mod main-stat :hit to-hit :two-handed false}
+        duelist-attack {:num 1 :sides 8 :mod (+ main-stat 2) :hit to-hit}
+        duelist-quarterstaff {:num 1 :sides 6 :mod (+ main-stat 2) :hit to-hit}
+        two-handed {:num 1 :sides 10 :mod main-stat :hit to-hit :two-handed true}
         number-of-attacks (cond
                             (< level 5) 1
                             (< level 11) 2
@@ -26,8 +28,12 @@
                :reaction true
                :defense-advantage false
                :defense-disadvantage false
-               ;:attacks (mapv (constantly two-handed) (range number-of-attacks))
-               :attacks (mapv (constantly melee-attack) (range number-of-attacks))
+               ;:attacks (mapv (constantly duelist-attack) (range number-of-attacks))
+               ;:attacks (mapv (constantly melee-attack) (range number-of-attacks))
+               ;:attacks (into (mapv (constantly two-handed) (range number-of-attacks))
+                              ;[{:num 1 :sides 4 :mod main-stat :hit to-hit :two-handed true}])
+               :attacks (into (mapv (constantly duelist-quarterstaff) (range number-of-attacks))
+                              [{:num 1 :sides 4 :mod (+ main-stat 2) :hit to-hit :two-handed false}])
                ;:attacks (mapv (constantly light-attack) (range (+ number-of-attacks 1))) ; two weapon fighting
                :max-hp max-hp
                :hp max-hp}}))
